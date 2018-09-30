64377eb651afd626363cfc53acdfb3f510811ffc
ashok <ashokpnd12@gmail.com>
2018-10-01 00:21:49 +0530

    ashok

--- HandsonTable/Controllers/HomeController.cs
+++ HandsonTable/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HandsonTable.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,17 @@ namespace HandsonTable.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee { empId=101,name="Ramzan Khan",designation="Software Developer",isPunctual=true,performance="Best",mobile="9675432468",ctc=3.5m,doj=new DateTime(01/03/2018) },
+                new Employee { empId=102,name="Suman Choudhury",designation="Software Developer",isPunctual=false,performance="Best",mobile="8764563278",ctc=3.4m,doj=new DateTime(15/02/2018) },
+                new Employee { empId=103,name="Ashok Pandey",designation="Software Developer",isPunctual=true,performance="Worst",mobile="9876567543",ctc=3.25m,doj=new DateTime(01/02/2018) }
+            };
+            return View(emp);
+        }
+
+        public ActionResult jQueryDemo()
         {
             return View();
         }
---
+++ HandsonTable/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HandsonTable.Models
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string name { get; set; }
+        public string designation { get; set; }
+        public bool isPunctual { get; set; }
+        public string performance { get;set; }
+        public string mobile { get; set; }
+        public decimal ctc { get; set; }
+        public DateTime doj { get; set; }
+    }
+}
